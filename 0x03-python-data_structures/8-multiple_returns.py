02e0f9f83c69183f5fc5b72a28e8d18eea892ad7
Chipatenii <innocentmanda70@gmail.com>
2024-01-08 18:57:28 -0800

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) == 0:
-        my_tuple = (0, None)
-    else:
-        my_tuple = (len(sentence), sentence[:1])
-    return(my_tuple)
+
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+    if not my_list:
+        return None
+    
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    
+    return max_value
