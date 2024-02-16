6c04c2fb34c9adc73d91a10565a9a858edcf7684
chipatenii <innocentmanda70@gmail.com>
2024-02-16 09:57:03 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -154,5 +154,5 @@ class Rectangle(Base):
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
+                                                      self.x, self.y,
                                                        self.width, self.height)
