9110bbb12774b168a5b19ae69eec636346065ae3
chipatenii <innocentmanda70@gmail.com>
2024-04-19 06:10:52 -0700

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-Class definition of a City
-"""
+"""Lists states"""
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from model_city import City
 
-# Create an instance of declarative_base
 Base = declarative_base()
 
-# Define the State class
+
 class State(Base):
+    """Class representing the states table"""
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-
-# Connect to MySQL server
-engine = create_engine('mysql://username:password@localhost:3306/database_name')
-
-# Create all tables defined by Base
-Base.metadata.create_all(engine)
