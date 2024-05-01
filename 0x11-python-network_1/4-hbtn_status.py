05388d914f0383f14403a991e721e033b3dd8339
Innocent Manda <106418148+Chipatenii@users.noreply.github.com>
2024-05-01 02:07:02 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""
-This script fetches https://alx-intranet.hbtn.io/status using the requests package.
-"""
-
-import requests
-
-
-def fetch_alx_intranet_status():
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
-
-
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+using requests"""
 if __name__ == "__main__":
-    fetch_alx_intranet_status()
+    import requests
+
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+          .format(type(r.text), r.text))
